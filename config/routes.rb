1bc8f7e451ccbf103a9e67303d690709a9c8a5d5
Lucca-mito <luccasukman@gmail.com>
2018-07-04 15:39:38 -0300

    Coisas

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,4 @@ Rails.application.routes.draw do
   resources :institutions
   resources :raffles
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
